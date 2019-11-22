be5f7e61ad97b6865b7d0ea9372f2c0d84170663
jason <wangzepeng@appropolis.com.cn>
2019-11-22 14:51:01 +0800

    config.js增加live2d相关配置

--- doc/README.md
+++ doc/README.md
@@ -13,6 +13,7 @@ features:
 - title: 高性能
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
 ---
+<!-- <canvas id="live2d" width="280" height="250" class="live2d"></canvas> -->
 <!-- 
 <ClientOnly>
   <BottomData/>
--- docs/live2dModels/live2d/js/init.js
+++ docs/live2dModels/live2d/js/init.js
@@ -1,3 +1,8 @@
 window.addEventListener('load',function(){
+  var canvas = document.createElement('canvas');
+  canvas.id = 'live2d';
+  canvas.width = '280';
+  canvas.height = '250';
+  document.body.appendChild(canvas);
   loadlive2d("live2d", "/wzp.github.io/live2dModels/live2d/model/kesshouban/model.json");
 })
